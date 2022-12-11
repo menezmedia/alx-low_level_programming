e9b097c1672d32d754da9df789a7ab66ac88b1a8
menezmedia <moseschineduofoegbu@gmail.com>
2022-12-11 09:48:50 -0800

    print tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
