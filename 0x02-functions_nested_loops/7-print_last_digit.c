87ba8443eabe2048d1c5d16f1afbc03c6227f68e
menezmedia <moseschineduofoegbu@gmail.com>
2022-12-14 02:48:42 -0800

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,14 @@
  */
 int print_last_digit(int n)
 {
-	int ld = n % 10;
+	int last;
 
-	if (ld < 0)
-		ld *= -1;
+	last = n % 10;
+	if (last < 0)
+	{
+	last = last * -1;
+	}
+	_putchar(last + '0');
 
-	_putchar(ld + '0');
-
-	return (0);
+	return (last);
 }
