805895751d49556affccf8447b9b58026bd1a56e
menezmedia <moseschineduofoegbu@gmail.com>
2022-12-14 01:11:34 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,13 @@
 /**
  * print_alphabet - prints the alpha in lowercase
  */
+
 void print_alphabet(void)
 {
 	char la;
 
-	for (la = 'a'; la <= 'z'; la++)
-	       _putchar(la);
+	for (la = 'a'; la <= 'z'; la++
+		_putchar(la);
 
 	_putchar('\n');
 }
