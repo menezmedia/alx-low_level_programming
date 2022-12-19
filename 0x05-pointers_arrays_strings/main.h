365c15c788890301a89aeb7867f359e22b32ad8f
menezmedia <moseschineduofoegbu@gmail.com>
2022-12-19 03:00:09 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * File - main.h
+ * Author: menezmedia
+ * Desc: Contains prototype for all funtions used in this project.
+ */
+
+int _putchar(char c);
+
+
+
+
+
+
+
+
+#endif 
